beb461116020e98c06b48a74aef304ba54472fce
msimp <max.simpson@gmail.com>
2021-09-04 13:25:50 -0500

    much handelbars progress

---
+++ public/js/login.js
@@ -0,0 +1,58 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector("#login_email").value.trim();
+    const password = document.querySelector("#login_password").value.trim();
+  
+    if (email && password) {
+      const response = await fetch("/api/users/login", {
+        method: "post",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/dashboard/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector("#signup_username").value.trim();
+    const email = document.querySelector("#signup_email").value.trim();
+    const password = document.querySelector("#signup_password").value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch("/api/users", {
+        method: "post",
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/dashboard/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document
+    .querySelector(".login_form")
+    .addEventListener("submit", loginFormHandler);
+  
+  document
+    .querySelector(".signup_form")
+    .addEventListener("submit", signupFormHandler);
+  
