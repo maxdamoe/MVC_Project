fc0d40caed2524de18ae5a62f0d879875c1af2f2
msimp <max.simpson@gmail.com>
2021-09-04 17:02:31 -0500

    FINALLY MYSQL IS WORKINGGGGG

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-const dashboardRoutes = require("./dashboard-routes.js");
-const homeRoutes = require("./homepage_routes.js");
+const dashboardRoutes = require("./dashboard_routes.js");
+const homePageRoutes = require("./homepage_routes.js");
 const apiRoutes = require("./api/");
 
 
-router.use("/", homeRoutes);
+router.use("/", homePageRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 
